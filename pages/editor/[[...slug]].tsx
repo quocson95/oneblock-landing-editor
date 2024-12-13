19c7f22155968cdadfd4fa21e21178f1db50b880
sondq <sondq@vng.com.vn>
2024-12-13 17:25:00 +0700

    fix

--- pages/editor/[[...slug]].tsx
+++ pages/editor/[[...slug]].tsx
@@ -75,7 +75,7 @@ const Editor =  () =>{
         <Button><Link href="/">Home</Link></Button>
         <Button  onClick={()=>{          saveMdx(parseInt(id[0]));        }} >Save</Button>        
         { saveStatus.length > 0 &&
-        <button type="button" class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"> {saveStatus}</button>
+        <button type="button" className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"> {saveStatus}</button>
         }
         <form>
           <div className="grid gap-6 mb-6 md:grid-cols-2">
