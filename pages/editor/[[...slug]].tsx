e5edba1624a91837db14a290db6b05d8be755c79
sondq <sondq@vng.com.vn>
2024-12-13 17:24:34 +0700

    update

--- pages/editor/[[...slug]].tsx
+++ pages/editor/[[...slug]].tsx
@@ -13,6 +13,7 @@ const Editor =  () =>{
     const [mdxContent, setMdxContent] = useState(' ');
     const [loading, setLoading] = useState(true);
     const [name, setName] = useState('');
+    const[saveStatus, setSaveStatus] = useState('')
     let didInit = false;
     const router = useRouter()
    
@@ -46,6 +47,11 @@ const Editor =  () =>{
       }
       const res = await fetch('https://api.oneblock.vn/be/mdx/?name='+saveName + "&id="+id, {method: "put", body: mdxContent,} )
       console.log(res);
+      if (res.status == 200) {
+        setSaveStatus('Save success!'); // Displays a success message
+      } else {
+        setSaveStatus('Save failed!'+ res.status); // Displays a success message
+      }
     }
 
     const copyContentMdx = (mdx: string) => {
@@ -67,8 +73,10 @@ const Editor =  () =>{
     return(
       <>
         <Button><Link href="/">Home</Link></Button>
-        <Button  onClick={()=>{          saveMdx(parseInt(id[0]));        }} >Save</Button>
-
+        <Button  onClick={()=>{          saveMdx(parseInt(id[0]));        }} >Save</Button>        
+        { saveStatus.length > 0 &&
+        <button type="button" class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"> {saveStatus}</button>
+        }
         <form>
           <div className="grid gap-6 mb-6 md:grid-cols-2">
           <div>
